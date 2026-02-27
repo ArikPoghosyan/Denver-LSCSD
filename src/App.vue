84e0a8a53c522b11ce0f9d355cea1caf0a77ab9b
Arik Poghosyan <65605361+ArikPoghosyan@users.noreply.github.com>
2026-02-28 01:29:08 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,3 @@
 import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
 </script>
-
-<style>
-/* Глобальные стили */
-:root {
-  --gold: #c6a756;
-  --navy: #080f16;
-  --white: #f0ede8;
-}
-
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  background: var(--navy);
-  color: var(--white);
-  font-family: 'Rajdhani', sans-serif;
-}
-
-#app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-</style>
